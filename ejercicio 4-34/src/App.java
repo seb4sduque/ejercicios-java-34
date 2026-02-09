53a27f9386e43bed14a7dcfd6e7cc83838ebb472
Duque <seb4sduque@gmail.com>
2026-02-09 12:45:50 -0500

    actualizacion, voy en ejercicio 5

--- ejercicio 4-34/src/App.java
+++ ejercicio 4-34/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    //Dados los catetos de un triángulo rectángulo, calcula su hipotenusa.
+        double cateto1 = 10;
+        double cateto2 = 5;
+        double hipotenusa = Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+        System.out.println("La hipotenusa es: " + hipotenusa);
     }
 }
---
+++ ejercicio 5-34/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    //Dados dos números, muestra la suma, resto, división y multiplicación de ambos.
+    int num1 = 10;
+    int num2 = 5;
+    int suma = num1 + num2;
+    int resta = num1 - num2;
+    int multiplicacion = num1 * num2;
+    int division = num1 / num2;
+    System.out.println("Suma: " + suma);
+    System.out.println("Resta: " + resta);
+    System.out.println("Multiplicación: " + multiplicacion);
+    System.out.println("División: " + division);
+    }
+}
